246eb3be67c5c39673b7e7b87b807d81dd814424
Apoulis <antonhspoulis@gmail.com>
2024-11-11 18:20:39 +0200

    Reapair Controller

--- TechnicoRMP.DatabaseNew/DataAccess/DataStore.cs
+++ TechnicoRMP.DatabaseNew/DataAccess/DataStore.cs
@@ -1,23 +1,43 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using TechnicoRMP.Models;
 
 namespace TechnicoRMP.Database.DataAccess;
 
 public class DataStore : DbContext
 {
+    public DataStore(DbContextOptions<DataStore> options) : base(options) {}
     public DbSet<User> Users { get; set; }
     public DbSet<PropertyItem> PropertyItems { get; set; }
     public DbSet<PropertyOwnership> PropertyOwnerships { get; set; }
     public DbSet<PropertyRepair> PropertyRepairs { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    {
-        string connectionString = "Data Source=(local);Initial Catalog=TechnicoDb; Integrated Security = True;TrustServerCertificate=True;";
-        optionsBuilder.UseSqlServer(connectionString);
-    }
+   
+
+    //public void ConfigureServices(IServiceCollection services)
+    //{
+    //    services.AddDbContext<DataStore>(options =>
+    //        options.UseSqlServer("Data Source=(local);Initial Catalog=TechnicoDb;Integrated Security=True;TrustServerCertificate=True;"));
+
+
+    //}
+
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+
+        modelBuilder.Entity<User>().HasData(
+           new User()
+           {
+               Id = 1,
+               Email = "abcd@gmail.com",
+               Name = "Ant",
+               Password = "12",  // ΠΡΟΣΟΧΗ: Κρυπτογράφησε τον κωδικό στην πραγματική εφαρμογή
+               Surname = "pl",
+               VatNumber = "122431234"
+           }
+       );
+
         modelBuilder
             .Entity<User>()
             .HasIndex(p => p.VatNumber)
--- TechnicoRMP.Shared/Common/Result.cs
+++ TechnicoRMP.Shared/Common/Result.cs
@@ -14,7 +14,6 @@ public class Result
 public class Result<T>
 {
     public int Status { get; set; }
-
     public string? Message { get; set; }
     public T? Value { get; set; }
 }
