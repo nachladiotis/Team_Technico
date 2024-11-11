fa239a0519cbba47454cf5055314d71fe9ee88da
iakovosl <iakovoslampadakis@gmail.com>
2024-11-11 21:30:29 +0200

    Validations

---
+++ Technico.Api/Validations/PropertyItemValidation.cs
@@ -0,0 +1,16 @@
+﻿using TechnicoRMP.Models;
+
+namespace Technico.Api.Validations
+{
+    public class PropertyItemValidation: IPropertyItemValidation
+    {
+        public bool PropertyItemValidator(PropertyItem propertyItem)
+        {
+            if (propertyItem == null)
+                return false;
+            if (propertyItem.YearOfConstruction == null || propertyItem.EnPropertyType == null)
+                return false;
+            return true;
+        }
+    }
+}
